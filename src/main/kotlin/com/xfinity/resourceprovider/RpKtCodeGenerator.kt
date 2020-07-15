20aa2e5dcb2be2cdbdc203d0a9b23e6f4116b7fc
dapp <daptronic@gmail.com>
2020-07-15 11:37:15 -0400

    Update kotlinpoet, correct imports for test utils

--- src/main/kotlin/com/xfinity/resourceprovider/RpKtCodeGenerator.kt
+++ src/main/kotlin/com/xfinity/resourceprovider/RpKtCodeGenerator.kt
@@ -1,61 +1,71 @@
 package com.xfinity.resourceprovider
 
 import com.squareup.kotlinpoet.ClassName
+import com.squareup.kotlinpoet.MemberName
 import com.squareup.kotlinpoet.FileSpec
 import com.squareup.kotlinpoet.FunSpec
 import java.io.File
 
 class RpKtCodeGenerator {
     fun generateTestUtils(receiverPackageName: String, generateIdsMock: Boolean, outputDirectoryName: String) {
-        val stringProviderAnswerClassName: ClassName = ClassName("com.xfinity.resourceprovider.testutils", "StringProviderAnswer")
-        val mockStringsFunSpec = FunSpec.builder("mockStrings")
-                .receiver(ClassName(receiverPackageName, "ResourceProvider"))
-                .addStatement(
-                        "com.nhaarman.mockito_kotlin.whenever(this.strings).thenReturn(" +
-                                "org.mockito.Mockito.mock($receiverPackageName.StringProvider::class.java, " +
-                                "%T()))", stringProviderAnswerClassName).build()
+        val resourceProviderClassName = ClassName(receiverPackageName, "ResourceProvider")
+        val stringProviderClassName = ClassName(receiverPackageName, "StringProvider")
+        val stringProviderAnswerClassName = ClassName("com.xfinity.resourceprovider.testutils", "StringProviderAnswer")
+        val wheneverMemberName = MemberName("com.nhaarman.mockitokotlin2", "whenever")
+        val mockMemberName = MemberName("org.mockito.Mockito", "mock")
 
+        val mockStringsFunSpec = FunSpec.builder("mockStrings")
+                .receiver(resourceProviderClassName)
+                .addStatement("%M(this.strings).thenReturn(" +
+                        "%M(%T::class.java, " +
+                        "%T()))", wheneverMemberName, mockMemberName, stringProviderClassName, stringProviderAnswerClassName).build()
+
+        val drawableProviderClassName = ClassName(receiverPackageName, "DrawableProvider")
         val drawableProviderAnswerClassName: ClassName = ClassName("com.xfinity.resourceprovider.testutils", "DrawableProviderAnswer")
         val mockDrawablesFunSpec = FunSpec.builder("mockDrawables")
-                .receiver(ClassName(receiverPackageName, "ResourceProvider"))
+                .receiver(resourceProviderClassName)
                 .addStatement(
-                        "com.nhaarman.mockito_kotlin.whenever(this.drawables).thenReturn(" +
-                                "org.mockito.Mockito.mock($receiverPackageName.DrawableProvider::class.java, " +
-                                "%T()))", drawableProviderAnswerClassName).build()
+                        "%M(this.drawables).thenReturn(" +
+                                "%M(%T::class.java, " +
+                                "%T()))", wheneverMemberName, mockMemberName, drawableProviderClassName, drawableProviderAnswerClassName).build()
 
+        val colorProviderClassName = ClassName(receiverPackageName, "ColorProvider")
         val integerProviderAnswerClassName: ClassName = ClassName("com.xfinity.resourceprovider.testutils", "IntegerProviderAnswer")
         val mockColorsFunSpec = FunSpec.builder("mockColors")
-                .receiver(ClassName(receiverPackageName, "ResourceProvider"))
+                .receiver(resourceProviderClassName)
                 .addStatement(
-                        "com.nhaarman.mockito_kotlin.whenever(this.colors).thenReturn(" +
-                                "org.mockito.Mockito.mock($receiverPackageName.ColorProvider::class.java, " +
-                                "%T()))", integerProviderAnswerClassName).build()
+                        "%M(this.colors).thenReturn(" +
+                                "%M(%T::class.java, " +
+                                "%T()))", wheneverMemberName, mockMemberName, colorProviderClassName, integerProviderAnswerClassName).build()
 
+        val dimensProviderClassName = ClassName(receiverPackageName, "DimensionProvider")
         val mockDimensFunSpec = FunSpec.builder("mockDimens")
-                .receiver(ClassName(receiverPackageName, "ResourceProvider"))
+                .receiver(resourceProviderClassName)
                 .addStatement(
-                        "com.nhaarman.mockito_kotlin.whenever(this.dimens).thenReturn(" +
-                                "org.mockito.Mockito.mock($receiverPackageName.DimensionProvider::class.java, " +
-                                "%T()))", integerProviderAnswerClassName).build()
+                        "%M(this.dimens).thenReturn(" +
+                                "%M(%T::class.java, " +
+                                "%T()))", wheneverMemberName, mockMemberName, dimensProviderClassName, integerProviderAnswerClassName).build()
 
+        val integerProviderClassName = ClassName(receiverPackageName, "IntegerProvider")
         val mockIntegersFunSpec = FunSpec.builder("mockIntegers")
-                .receiver(ClassName(receiverPackageName, "ResourceProvider"))
+                .receiver(resourceProviderClassName)
                 .addStatement(
-                        "com.nhaarman.mockito_kotlin.whenever(this.integers).thenReturn(" +
-                                "org.mockito.Mockito.mock($receiverPackageName.IntegerProvider::class.java, " +
-                                "%T()))", integerProviderAnswerClassName)
+                        "%M(this.integers).thenReturn(" +
+                                "%M(%T::class.java, " +
+                                "%T()))", wheneverMemberName, mockMemberName, integerProviderClassName, integerProviderAnswerClassName)
                 .build()
 
+        val idProviderClassName = ClassName(receiverPackageName, "IdProvider")
         val mockIdsFunSpec = FunSpec.builder("mockIds")
-                .receiver(ClassName(receiverPackageName, "ResourceProvider"))
+                .receiver(resourceProviderClassName)
                 .addStatement(
-                        "com.nhaarman.mockito_kotlin.whenever(this.ids).thenReturn(" +
-                                "org.mockito.Mockito.mock($receiverPackageName.IdProvider::class.java, " +
-                                "%T()))", integerProviderAnswerClassName)
+                        "%M(this.ids).thenReturn(" +
+                                "%M(%T::class.java, " +
+                                "%T()))", wheneverMemberName, mockMemberName, idProviderClassName, integerProviderAnswerClassName)
                 .build()
 
         val mockFunSpecBuilder = FunSpec.builder("mock")
-                .receiver(ClassName(receiverPackageName, "ResourceProvider"))
+                .receiver(resourceProviderClassName)
                 .addStatement("this.mockStrings()")
                 .addStatement("this.mockDrawables()")
                 .addStatement("this.mockColors()")
